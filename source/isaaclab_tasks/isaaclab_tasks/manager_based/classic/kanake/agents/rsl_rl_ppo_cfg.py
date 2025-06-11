f1e32279ebdc18ce57a03cf074952d4ba71f7973
seosangryeong <seosang990911@gmail.com>
2025-06-11 10:31:28 +0900

    read

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/classic/kanake/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/classic/kanake/agents/rsl_rl_ppo_cfg.py
@@ -35,3 +35,18 @@ class kanakePPORunnerCfg(RslRlOnPolicyRunnerCfg):
         desired_kl=0.0,
         max_grad_norm=1.0,
     )
+
+    # algorithm = RslRlPpoAlgorithmCfg(
+    #     value_loss_coef=1.0,
+    #     use_clipped_value_loss=True,
+    #     clip_param=0.2,
+    #     entropy_coef=0.005,
+    #     num_learning_epochs=5,
+    #     num_mini_batches=4,
+    #     learning_rate=1.0e-3,
+    #     schedule="adaptive",
+    #     gamma=0.99,
+    #     lam=0.95,
+    #     desired_kl=0.01,
+    #     max_grad_norm=1.0,
+    # )
